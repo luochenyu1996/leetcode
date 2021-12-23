75c984683b0e7fc137f7919789df528d4711ac5a
chenyu <luochenyu1996@qq.com>
2021-12-23 15:48:38 +0800

    '增加题解'

---
+++ src/Solution1034.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+/**
+* 1034. 边界着色
+*@author chen yu
+*@create 2021-12-23 11:55
+*/
+public class Solution1034 {
+}
---
+++ src/Solution1254.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+/**
+* 1254. 统计封闭岛屿的数目
+*@author chen yu
+*@create 2021-12-23 10:50
+*/
+public class Solution1254 {
+}
